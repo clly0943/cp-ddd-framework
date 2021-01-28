ca1b0de57e36d2de9dc1c522e1df0e6c102ecc28
liuzebin <c_k_lau@163.com>
2021-01-28 10:36:32 +0800

    test Signed-off-by: liuzebin <c_k_lau@163.com>

--- dddplus-test/src/test/java/io/github/dddplus/runtime/ReducerTest.java
+++ dddplus-test/src/test/java/io/github/dddplus/runtime/ReducerTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
